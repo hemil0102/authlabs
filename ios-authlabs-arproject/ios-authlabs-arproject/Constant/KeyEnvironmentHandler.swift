6f311168738dc2da36a9bccd8370254182d44bf0
hemil0102 <hemil0102@gmail.com>
2024-04-30 22:03:45 +0900

    feat: APIKey 관리용 KeyEnvironmentHandler 구현

---
+++ ios-authlabs-arproject/ios-authlabs-arproject/Constant/KeyEnvironmentHandler.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+protocol APIKey {
+    static var OpenAIAPIKey: String { get }
+    static var GoogleAPIKey: String { get }
+}
+
+struct Utils: APIKey {
+    static var OpenAIAPIKey: String {
+        guard let filePath = Bundle.main.path(forResource: "DEBUG-Keys", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath) else { return "" }
+        return plist.object(forKey: "API_KEY") as? String ?? ""
+    }
+    
+    static var GoogleAPIKey: String {
+        guard let filePath = Bundle.main.path(forResource: "DEBUG-Keys", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath) else { return "" }
+        return plist.object(forKey: "GOOGLE_API_KEY") as? String ?? ""
+    }
+    
+    static var GoogleCXKey: String {
+        guard let filePath = Bundle.main.path(forResource: "DEBUG-Keys", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath) else { return "" }
+        return plist.object(forKey: "GOOGLE_CX_KEY") as? String ?? ""
+    }
+}
+
